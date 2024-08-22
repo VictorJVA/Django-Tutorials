7f2957850a8a165e2b48f035de47c6e15434f801
VictorJVA <vjvilladia@eafit.edu.co>
2024-08-22 00:10:59 -0500

    Add and comment product

--- djangocourse/helloworld/pages/views.py
+++ djangocourse/helloworld/pages/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, ListView
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ValidationError
+from django.urls import reverse
 from django.views import View
 from django import forms
+from .models import Product
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -36,13 +40,6 @@ class AboutContactView(TemplateView):
         })
         return context
 
-class Product:
-    products = [
-        {"id": "1", "name": "TV", "description": "Best TV", "price": 1000},
-        {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 1100},
-        {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 50},
-        {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 100},
-    ]
 
 class ProductIndexView(View):
     template_name = 'products/index.html'
@@ -51,23 +48,37 @@ class ProductIndexView(View):
         viewData = {}
         viewData["title"] = "Products - Online Store"
         viewData["subtitle"] = "List of products"
-        viewData["products"] = Product.products
+        viewData["products"] = Product.objects.all()
         return render(request, self.template_name, viewData)
 
 class ProductShowView(View):
     template_name = 'products/show.html'
 
     def get(self, request, id):
+        try:
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Product ID must be greater 1 or greater")
+            product = get_object_or_404(Product, id=product_id)
+        except ValueError as e:
+            return HttpResponseRedirect(reverse('home'))
         viewData = {}
-        product = Product.products[int(id)-1]
-        viewData["title"] = product["name"] + " - Online Store"
-        viewData["subtitle"] = product["name"] + " Product information"
+        product = get_object_or_404(Product, id=product_id)
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " Product information"
         viewData["product"] = product 
         return render(request, self.template_name, viewData)
 
 class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True)
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None and price <= 0:
+            raise ValidationError('Price must be greater than zero.')
+        return price
 
 class ProductCreateView(View):
     template_name = 'products/create.html'
@@ -86,13 +97,24 @@ class ProductCreateView(View):
             # Process the form data here
             # For example, save the data to the database
             # product = Product(name=form.cleaned_data['name'], price=form.cleaned_data['price'])
-            # product.save()
+            form.save()
             
             # Redirect to a success page or another view
-            return redirect('product_list')  # Replace with the actual URL name or path
+            return redirect('product-created')  # Replace with the actual URL name or path
         else:
             viewData = {
                 "title": "Create Product",
                 "form": form
             }
-            return render(request, self.template_name, viewData)    
+            return render(request, self.template_name, viewData)
+
+class ProductUpdateView(View):
+    model = Product
+    template_name = 'products_list.html' 
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Products - Online Store'
+        context['subtitle'] = 'List of products'
+        return context  
