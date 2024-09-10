0bc53619a5fc02480ceac08da42682d06d631e82
VictorJVA <vjvilladia@eafit.edu.co>
2024-09-09 22:06:41 -0500

    feat: dependency i.p applicated

---
+++ djangocourse/helloworld/pages/interfaces.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from django.http import HttpRequest
+
+class ImageStorage(ABC):
+
+    @abstractmethod
+    def store(self, request: HttpRequest, image) -> str:
+        pass
+
+    
--- djangocourse/helloworld/pages/views.py
+++ djangocourse/helloworld/pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView, ListView
 from django.http import HttpResponseRedirect
 from django.core.exceptions import ValidationError
@@ -6,7 +7,7 @@ from django.urls import reverse
 from django.views import View
 from django import forms
 from .models import Product
-
+from .utils import ImageLocalStorage
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -120,17 +121,19 @@ class CartView(View):
     template_name = 'cart/index.html'
 
     def get(self, request):
-        products = {}
-        products[121] = {"name": "Tv Samsung", "price": 1000}
-        products[11] = {"name": "Iphone", "price": 2000}
+        # Obtener los datos de los productos disponibles
+        products = {
+            121: {'name': 'Tv Samsung', 'price': 1000},
+            11: {'name': 'Iphone', 'price': 2000}
+        }
 
-        cart_products = {}
-        cart_product_data = request.session.get('cart_product_data', {})    
+        # Obtener los productos en el carrito de la sesión
+        cart_product_data = request.session.get('cart_product_data')
+        if cart_product_data is None:
+            cart_product_data = {}
+        cart_products = {key: product for key, product in products.items() if str(key) in cart_product_data}
 
-        for key, product in products.items():
-            if str(key) in cart_product_data.keys():
-                cart_products[key] = product
-
+        # Pasar los productos del carrito a la vista
         view_data = {
             "title": "Cart - Online Store",
             "subtitle": "Shopping cart",
@@ -138,14 +141,16 @@ class CartView(View):
         }
 
         return render(request, self.template_name, view_data)
+
     
 
     def post(self, request, product_id):
-        cart_product_data = request.session.get('cart_product_data', {})
-        cart_product_data[product_id] = product_id
-        request.session['cart_product_data'] = cart_product_data
+            cart_product_data = request.session.get('cart_product_data')
+            cart_product_data[product_id] = product_id
+            request.session['cart_product_data'] = cart_product_data
 
-        return redirect('cart_index')
+            return redirect('cart_index')
+
     
 class CartRemoveAllView(View):
     def post(self, request):
@@ -154,3 +159,31 @@ class CartRemoveAllView(View):
 
         return redirect('cart_index')
 
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = 'images/index.html'
+
+        def get(self, request):
+            image_url = request.session.get('image_url', "")
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('image_index')
+        
+    return ImageView
+
+
+class ImageViewNoDI(View):
+    template_name = 'imagesnotdi/index.html'
+
+    def get(self, request):
+        image_url = request.session.get('image_url', "")
+        return render(request, self.template_name, {'image_url': image_url})
+
+    def post(self, request):
+        image_storage = ImageLocalStorage()
+        image_url = image_storage.store(request)
+        request.session['image_url'] = image_url
+        return redirect('image_index')
